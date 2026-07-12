17149d9d1c445738935badf079953c8500538b4a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "private_dns_resolver_outbound_endpoints" {
-  description = "All private_dns_resolver_outbound_endpoint resources"
-  value       = azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints
-}
 output "private_dns_resolver_outbound_endpoints_location" {
-  description = "List of location values across all private_dns_resolver_outbound_endpoints"
-  value       = [for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : v.location]
+  description = "Map of location values across all private_dns_resolver_outbound_endpoints, keyed the same as var.private_dns_resolver_outbound_endpoints"
+  value       = { for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : k => v.location }
 }
 output "private_dns_resolver_outbound_endpoints_name" {
-  description = "List of name values across all private_dns_resolver_outbound_endpoints"
-  value       = [for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : v.name]
+  description = "Map of name values across all private_dns_resolver_outbound_endpoints, keyed the same as var.private_dns_resolver_outbound_endpoints"
+  value       = { for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : k => v.name }
 }
 output "private_dns_resolver_outbound_endpoints_private_dns_resolver_id" {
-  description = "List of private_dns_resolver_id values across all private_dns_resolver_outbound_endpoints"
-  value       = [for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : v.private_dns_resolver_id]
+  description = "Map of private_dns_resolver_id values across all private_dns_resolver_outbound_endpoints, keyed the same as var.private_dns_resolver_outbound_endpoints"
+  value       = { for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : k => v.private_dns_resolver_id }
 }
 output "private_dns_resolver_outbound_endpoints_subnet_id" {
-  description = "List of subnet_id values across all private_dns_resolver_outbound_endpoints"
-  value       = [for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : v.subnet_id]
+  description = "Map of subnet_id values across all private_dns_resolver_outbound_endpoints, keyed the same as var.private_dns_resolver_outbound_endpoints"
+  value       = { for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : k => v.subnet_id }
 }
 output "private_dns_resolver_outbound_endpoints_tags" {
-  description = "List of tags values across all private_dns_resolver_outbound_endpoints"
-  value       = [for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : v.tags]
+  description = "Map of tags values across all private_dns_resolver_outbound_endpoints, keyed the same as var.private_dns_resolver_outbound_endpoints"
+  value       = { for k, v in azurerm_private_dns_resolver_outbound_endpoint.private_dns_resolver_outbound_endpoints : k => v.tags }
 }
 
